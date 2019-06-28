ba3433197b7540bf26cc8741cacb58f4f359a062
João Paulo Moraes <joaopaulomoraes@outlook.com>
2019-06-28 15:31:48 -0300

    refactor: Simplified component with ServerStyleSheets

--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,17 @@
 import React, { Fragment } from 'react'
-import PropTypes from 'prop-types'
 import Document, { Head, Main, NextScript } from 'next/document'
-import flush from 'styled-jsx/server'
+import { ServerStyleSheets } from '@material-ui/styles'
+import theme from '../src/utils/theme'
 
 class _Document extends Document {
   render () {
-    const { pageContext } = this.props
-
     return (
       <html lang='pt-BR' dir='ltr'>
         <Head>
-          <meta charSet='utf-8' />
-          <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no' />
-          <meta name='theme-color' content={pageContext ? pageContext.theme.palette.primary.main : null} />
-          <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500' />
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no" />
+          <meta name="theme-color" content={theme.palette.primary.main} />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
         </Head>
         <body>
           <Main />
@@ -24,39 +22,22 @@ class _Document extends Document {
   }
 }
 
-_Document.getInitialProps = ctx => {
-  let pageContext
+_Document.getInitialProps = async ctx => {
+  const sheets = new ServerStyleSheets()
+  const originalRenderPage = ctx.renderPage
 
-  const page = ctx.renderPage(Component => {
-    const WrappedComponent = props => {
-      pageContext = props.pageContext
-      return <Component {...props} />
-    }
-
-    WrappedComponent.propTypes = {
-      pageContext: PropTypes.object.isRequired
-    }
-
-    return WrappedComponent
+  ctx.renderPage = () => originalRenderPage({
+    enhanceApp: WrappedComponent => props => sheets.collect(<WrappedComponent {...props} />)
   })
 
-  let css
-
-  if (pageContext) {
-    css = pageContext.sheetsRegistry.toString()
-  }
+  const initialProps = await Document.getInitialProps(ctx)
 
   return {
-    ...page,
-    pageContext,
-
+    ...initialProps,
     styles: (
       <Fragment>
-        <style
-          id='jss-server-side'
-          dangerouslySetInnerHTML={{ __html: css }}
-        />
-        {flush() || null}
+        {initialProps.styles}
+        {sheets.getStyleElement()}
       </Fragment>
     )
   }
