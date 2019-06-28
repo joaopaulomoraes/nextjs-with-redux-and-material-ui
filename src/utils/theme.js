81ccc5ee9115b8a614fe29e23d096e197fa5fede
João Paulo Moraes <joaopaulomoraes@outlook.com>
2019-06-28 15:29:26 -0300

    refactor: Created a theme instead page context

---
+++ src/utils/theme.js
@@ -0,0 +1,10 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+
+const theme = createMuiTheme({
+  palette: {
+    // Customize Material-UI with your theme
+    // See more here: https://material-ui.com/customization/themes/
+  }
+})
+
+export default theme
